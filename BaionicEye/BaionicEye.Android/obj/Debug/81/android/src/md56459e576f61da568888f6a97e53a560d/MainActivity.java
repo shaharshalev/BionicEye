0727544af2e48da310e4edd7425137b10fae152f
shaharshalev <shaharshalev@campus.technion.ac.il>
2019-01-11 11:23:41 +0200

    initial commit of android app

---
+++ BaionicEye/BaionicEye.Android/obj/Debug/81/android/src/md56459e576f61da568888f6a97e53a560d/MainActivity.java
@@ -0,0 +1,47 @@
+package md56459e576f61da568888f6a97e53a560d;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("BaionicEye.Droid.MainActivity, BaionicEye.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("BaionicEye.Droid.MainActivity, BaionicEye.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ BaionicEye/BaionicEye/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+
+namespace BaionicEye
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        
+
+        
+
+
+        private async void RecordButton_Clicked(object sender, EventArgs e)
+        {
+            
+            
+            
+            
+
+        }
+    }
+}
